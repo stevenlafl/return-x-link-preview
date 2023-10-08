d9c2491f068a7e7f9b25a59fabce3fc1ba420211
Steven Linn <smlucf@gmail.com>
2023-10-08 12:57:40 -0600

    Add packaging script and remove unnecessary depenency

---
+++ package.ps1
@@ -0,0 +1,17 @@
+pnpm build
+pnpm package
+
+pnpm build --target=firefox-mv2
+pnpm package --target=firefox-mv2
+
+pnpm build --target=edge-mv3
+pnpm package --target=edge-mv3
+
+pnpm build --target=brave-mv3
+pnpm package --target=brave-mv3
+
+pnpm build --target=opera-mv3
+pnpm package --target=opera-mv3
+
+pnpm build --target=safari-mv3
+pnpm package --target=safari-mv3
